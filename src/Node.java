72e291d63b353796d3429c17945a36ab6af6a0eb
fgg12 <fgg12@FGG>
2015-02-02 17:56:23 +0200

    .

---
+++ src/Node.java
@@ -0,0 +1,100 @@
+
+public class Node<T>{
+
+	private Node<T> previousNode;
+	private Node<T> nextNode;
+	private T value;
+	
+	public Node (){
+		
+	}
+	
+	public Node(T value){
+		this.value = value;
+	}
+	
+	public Node(Node<T> node){
+		this.previousNode = null;
+		this.nextNode = null;
+	}
+
+	public Node<T> getPreviousNode() {
+		return previousNode;
+	}
+
+	public boolean HasPreviousNode() {
+		return (previousNode != null);
+	}
+	
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+	
+	public boolean HasNextNode() {
+		return (nextNode != null);
+	}
+
+	private void setPreviousNode(Node<T> previousNode) {
+		this.previousNode = previousNode;
+	}
+
+	private void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	public void addNextNode(Node<T> nextNode) {
+		if(this == nextNode) return;
+		if (getNextNode() == null) {
+			setNextNode(nextNode);
+			setPreviousNode(nextNode.getPreviousNode());
+			if (nextNode.getPreviousNode() == null) {
+				nextNode.setPreviousNode(this);
+			}else{
+				nextNode.getPreviousNode().setNextNode(this);
+				nextNode.setPreviousNode(this);
+			}
+		}else{
+			Node<T> temp = getNextNode();
+			setNextNode(nextNode);
+			getNextNode().setNextNode(temp);
+			temp.setPreviousNode(this.getNextNode());
+			getNextNode().setPreviousNode(this);
+		}
+	}
+	
+	public void addPreviousNode(Node<T> previousNode){
+		if(this == nextNode) return;
+		if (getPreviousNode() == null) {
+			setPreviousNode(previousNode);
+			previousNode.setNextNode(this);
+		}else{
+			Node<T> temp = getPreviousNode();
+			setPreviousNode(previousNode);
+			getPreviousNode().setNextNode(this);
+			temp.setNextNode(previousNode);
+			getPreviousNode().setPreviousNode(temp);
+		}
+	}
+
+	public void deleteNode(){
+		if (getPreviousNode() != null) {
+			getPreviousNode().setNextNode(getNextNode());			
+		}
+		if (getNextNode() != null) {
+			getNextNode().setPreviousNode(getPreviousNode());			
+		}
+		setPreviousNode(null);
+		setNextNode(null);
+	}
+	
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+
+
+}
